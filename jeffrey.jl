5e441577edc7e90cd1ba7ef133b655cc14f919a7
Jeffrey Sun <jeffreyesun@gmail.com>
2019-04-04 13:40:33 -0400

    Hopefully fixed something

--- jeffrey.jl
+++ jeffrey.jl
@@ -1,5 +1,5 @@
-using Statistics
-using DataFrames, Combinatorics, KernelDensity
+using Statistics, Distributions
+using DataFrames, Combinatorics, KernelDensity, Dierckx
 import CSV, Plots
 
 bids = CSV.File("FPSB_data.csv") |> DataFrame |> Matrix{Float64}
@@ -13,23 +13,33 @@ end
 
 function Q2()
     m_i(i) = maximum(bids[:,i.!=1:3], dims=2)[:,1]
-    Gb_i(bid, i) = mean(m_i(i) .<= bid)
-    kde_vec = [kde(m_i(i), bandwidth=5) for i=1:3]
-    g(bid, i) = pdf(kde_vec[i], bid)
-    u = hcat([bids[:,i] + Gb_i.(bids[:,i], i)./g(bids[:,i], i) for i=1:3]...)
-    f(bid, i) = pdf(kde(u[:,i][.!isnan.(u[:,i])], bandwidth=10), bid)
-    Plots.plot(50:250, [f(50:250, i) for i=1:3], xlabel="bid", ylabel="f", label=[1,2,3], title="Affiliated Private Values")
+    u = zeros(1000,3)
+    for i=1:3
+        function G_hat_i(bid)
+            m_i_mask = m_i(i) .<= bid
+            sum(m_i_mask) < 3 && (return 0)
+            mean(m_i_mask)*pdf(kde(bids[:,i][m_i_mask]), bid)
+        end
+        g_hat_kde = kde((bids[:,i], m_i(i)))
+        g_hat_i(bid) = pdf(g_hat_kde, bid, bid)
+        u[:,i] = bids[:,i] + G_hat_i.(bids[:,i])./g_hat_i.(bids[:,i])
+    end
+    f_kde_vec = [kde(u[:,i][(.!isnan.(u[:,i])) .& (u[:,i].<1000)]) for i=1:3]
+    f(bid, i) = pdf(f_kde_vec[i], bid)
+    Plots.plot(50:.1:300, [f(50:.1:300, i) for i=1:3], xlabel="bid", ylabel="f", label=[1,2,3], title="Affiliated Private Values")
 end
 
 function Q3()
     Fb_j(bid, j) = mean(bids[:,j] .<= bid)
-    Gb_i(bid, i) = prod([Fb_j(bid, j) for j=(1:3)[i.!=1:3]])
-    kde_vec = [kde(bids[:,j], bandwidth=5) for j=1:3]
-    fb_j(bid, j) = pdf(kde_vec[j], bid)
-    gb_i(bid, i) = prod([fb_j(bid, j) for j=(1:3)[i.!=1:3]])
-    u = hcat([bids[:,i] + Gb_i.(bids[:,i], i)./gb_i.(bids[:,i], i) for i=1:3]...)
-    f(bid, i) = pdf(kde(u[:,i][.!isnan.(u[:,i])], bandwidth=400), bid)
-    Plots.plot(0:10000, [f(0:10000, i) for i=1:3], xlabel="bid", ylabel="f", label=[1,2,3], title="Independent Private Values")
+    u = zeros(1000,3)
+    for i=1:3
+        Gb_i(bid) = prod([Fb_j(bid, j) for j=(1:3)[i.!=1:3]])
+        m_samp = maximum(hcat([sample(bids[:,j],50000) for j=(1:3)[i!=1:3]]...), dims=2)[:,1]
+        gb_kde = kde(m_samp)
+        u[:,i] = bids[:,i] + Gb_i.(bids[:,i])./pdf(gb_kde, bids[:,i])
+    end
+    f(bid, i) = pdf(kde(u[:,i][.!isnan.(u[:,i])]), bid)
+    Plots.plot(0:300, [f(0:300, i) for i=1:3], xlabel="bid", ylabel="f", label=[1,2,3], title="Independent Private Values")
 end
 
 Plots.png(Q2(), "Q1.2")
